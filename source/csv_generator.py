a704a4db643c901eea49cec1c33953b21c82b19a
cslev <cslev@gmx.com>
2020-03-18 09:38:12 +0800

    modify script to get the SSLKEY path from OS environment variables

---
+++ source/csv_generator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import os
+
+#getting the ENV files for the SSLKEYLOG
+SSLKEY   = os.getenv('SSLKEYLOG')
+SSLDEBUG = os.getenv('SSLDEBUG')
+
+# ## tshark -r capture-1-200 -Y "http2" -o tls.keylog_file:sslkey1.log -T fields -e frame.number -e _ws.col.Time -e ip.src -e ip.dst -e _ws.col.Protocol -e frame.len -e _ws.col.Info -E header=y -E separator="," -E quote=d -E occurrence=f > test1.csv
+
+
+
+
+## here in the parameter of os.walk, specify the location of the folder containing the pcaps
+for _,_,files in os.walk("./CB2/") :
+    print(files)
+
+
+total = len(files)
+count = 1 
+print("Converting .pcap files to .csv")
+for f in files :
+    file_name = "./CB2/" + f ;
+    output_file_name = f.split('-')[1] + "-" + f.split('-')[2] +".csv"
+    print(output_file_name)
+    
+    ## here in tls.keylog_file: speciy location and name of sslkeylogfile
+    csv_command = 'tshark -r ' + file_name +' -Y "http2" -o tls.keylog_file:'+SSLKEY+' -T fields -e frame.number -e _ws.col.Time -e ip.src -e ip.dst -e _ws.col.Protocol -e frame.len -e _ws.col.Info -E header=y -E separator="," -E quote=d -E occurrence=f > '+ output_file_name
+    os.system(csv_command)
+    print(str(count) + " of " + str(total) + " completed!")
+    count+=1
+print("finished!")
+
+
+
+
+
