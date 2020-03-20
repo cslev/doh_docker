aa32f0bfbbdf33b324e36088237694b00eaf7836
Himanshu <heman.sam@gmail.com>
2020-03-20 12:05:44 +0800

    Changes made to doh_capture.py to make it compatible with Dockerfile.autostart

--- Dockerfile
+++ Dockerfile
@@ -2,42 +2,88 @@ FROM debian:bullseye
 LABEL maintainer="cslev <cslev@gmx.com>"
 
 #packages needed for compilation
-ENV DEPS apt-utils \
-	 tshark \
+ENV DEPS tshark \
 	 tcpdump \
 	 nano \
-	 net-tools \
+#	 net-tools \
          tar \
 	 bzip2 \
-	 xz-utils \
-	 wget
+#	 xz-utils \
+	 wget \
+	 gconf-service \
+	 libasound2 \
+         libatk1.0-0 \
+         libc6 libcairo2 \
+         libcups2 \
+         libdbus-1-3 \
+         libexpat1 \
+         libfontconfig1 \
+	 libgcc1 \
+         libgconf-2-4 \
+         libgdk-pixbuf2.0-0 \
+         libglib2.0-0 \
+         libgtk-3-0 \
+         libnspr4 \
+         libpango-1.0-0 \
+         libpangocairo-1.0-0 \
+         libstdc++6 \
+         libx11-6 \
+         libx11-xcb1 \
+         libxcb1 \
+         libxcomposite1 \
+         libxcursor1 \
+         libxdamage1 \
+         libxext6 \
+         libxfixes3 \
+         libxi6 \
+         libxrandr2 \
+         libxrender1 \
+         libxss1 \
+         libxtst6 \
+         ca-certificates \
+         fonts-liberation \
+         libappindicator1 \
+         libnss3 \
+         lsb-release \
+         xdg-utils \
+	 libxt6 
 
 ENV PYTHON_DEPS  python3 \
-	         python3-pip \
-		 libpython3-dev \
+#	         python3-pip \
+		 python3-six \
 		 python3-pandas \
-		 python3-numpy \
-		 python3-selenium
+		 libpython3-dev 
+#		 python3-numpy
+#		 python3-selenium
+# we downgrade selenium to 3.14.1 as bullseye debian has the alpha 4.0, which does not work properly now.
 
 
 COPY source /doh_project
 WORKDIR /doh_project
-
 SHELL ["/bin/bash", "-c"]
 RUN apt-get update && \
     apt-get install -y --no-install-recommends $DEPS && \
     apt-get install -y --no-install-recommends $PYTHON_DEPS && \
+    dpkg -i selenium/python3-urllib3_1.24.1.deb && \
+    dpkg -i selenium/python3-selenium_3.14.1.deb && \
     apt-get autoremove --purge -y && \
     wget -q https://ftp.mozilla.org/pub/firefox/releases/74.0/linux-x86_64/en-US/firefox-74.0.tar.bz2 && \
     tar -xjf firefox-74.0.tar.bz2 && \
     tar -xzf geckodriver-v0.26.0-linux64.tar.gz && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* && \
+    rm -rf selenium/ && \
+    rm -rf firefox-74.0.tar.bz2 && \
+    rm -rf geckodriver-v0.26.0-linux64.tar.gz && \
     chmod +x geckodriver && \
     chmod +x doh_capture.py && \
-    cp bashrc_template /root/.bashrc && \
+    cp geckodriver /usr/bin && \
+    mkdir -p /usr/lib/firefox && \
+    ln -s /doh_project/firefox/firefox /usr/lib/firefox/firefox && \
+    mv others/bashrc_template /root/.bashrc && \
     source /root/.bashrc && \
-    echo "DoH docker image is ready"
+    mkdir -p pcap 
+
 
 
 
--- source/doh_capture.py
+++ source/doh_capture.py
@@ -1,16 +1,52 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+
 import os 
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException , WebDriverException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary # for specifying the path to firefox binary
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep,ctime
 import multiprocessing
 import time
+import argparse
 
-start = 1 
-stop = 5000
-batch_size = 200
+# parser for the command line arguements
+parser = argparse.ArgumentParser(description="DoH packet capture and .csv conversion script!")
+
+parser.add_argument('-s', action="store", default=1, type=int, dest="start" , help="Specify rank of the starting website")
+parser.add_argument('-e', action="store", default=200, type=int, dest="end" , help="Specify rank of the ending website")
+parser.add_argument('-b', action="store", default=200, type=int, dest="batch" , help="Batch Size (range must be a multiple of batch size!)")
+parser.add_argument('-r', action="store", default=1, type=int, dest="doh_resolver" , help="DoH resolver :\n1=Cloudflare; \n2=Google;\n3=CleanBrowsing;\n4=Quad9;")
+
+results = parser.parse_args()
+
+print("Printing script Parameters: ")
+print("Start = "+str(results.start))
+print("End = "+str(results.end))
+print("Batch_Size = "+str(results.batch))
+
+if(results.doh_resolver==1) :
+    uri="https://cloudflare-dns.com/dns-query"
+elif(results.doh_resolver==2) :
+    uri="https://dns.google/dns-query"
+elif(results.doh_resolver==3) :
+    uri="https://doh.cleanbrowsing.org/doh/family-filter/"
+elif(results.doh_resolver==4) :
+    uri="https://dns.quad9.net/dns-query"
+else :
+    print("Invalid choice for DoH resolver!\nExiting...")
+    exit(0)
+
+print("DoH_Resolver = "+uri)
+
+
+start = results.start 
+stop = results.end
+batch_size = results.batch
 time_out = batch_size * 15
 
 data = pd.read_csv('top-1m.csv' , names = ['rank','website']) 
@@ -24,18 +60,22 @@ options.headless = True
 ## specifying the binary path
 binary = FirefoxBinary('./firefox/firefox')
 
+## DesiredCapabilities
+##cap = DesiredCapabilities().FIREFOX
+##cap['marionette'] = False
+
 #profile = webdriver.FirefoxProfile('/home/user/.mozilla/firefox/0b5055qu.Doh_profile')
 ## setting the firefox profile to use DoH
 profile = webdriver.FirefoxProfile()
 profile.set_preference("network.trr.mode", 2)
-profile.set_preference("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query")
-profile.set_preference("network.trr.bootstrapAddress", '1.1.1.1.')
+profile.set_preference("network.trr.uri", uri)
+#profile.set_preference("network.trr.bootstrapAddress", '1.1.1.1.')
 
 def open_website(url):
     #driver = webdriver.Firefox(firefox_profile=profile)
 
     ## in the executabel path you need to specify the location of geckodriver location.
-    driver = webdriver.Firefox(executable_path='./geckodriver',firefox_binary=binary, options=options, firefox_profile=profile)
+    driver = webdriver.Firefox(options=options, firefox_profile=profile)
     driver.set_page_load_timeout(25) 
     try :
         driver.get(url)
@@ -71,10 +111,10 @@ def capture_packets(shell_command) :
     os.system(shell_command)
 
 while(e<=stop) :
-    filename = 'capture-'+str(s)+'-'+str(e)
+    filename = 'pcap/capture-'+str(s)+'-'+str(e)
 
     ## here after -i you need to add the ethernet port. which i guess is eth0
-    shell_command = "sudo timeout 4400 tcpdump port 443 -i eth0 -w " + filename
+    shell_command = "timeout 4400 tcpdump port 443 -i eth0 -w " + filename
 
     t1 = multiprocessing.Process(target=main_driver, args=(s,e,))
     t2 = multiprocessing.Process(target=capture_packets, args=(shell_command,))
@@ -92,8 +132,11 @@ while(e<=stop) :
     print(time.ctime())
     s = s+batch_size
     e = e+batch_size
-
+    
+    csv_command = "python3 csv_generator.py"
+    os.system(csv_command)
+    sleep(1)
     # this part is for moving the files to another location which is not needed in case of the docker container.
-    """move_command = "sudo mv "+filename+" /mnt/debianDoH_images/DoH_Pcaps/debianDoH2/"+filename
+    """move_command = "mv "+filename+" /mnt/debianDoH_images/DoH_Pcaps/debianDoH2/"+filename
     os.system(move_command)
     print("File moved to /mnt") """
