3e80786e91c45d241a139d31931e0597a19dd958
cslev <cs.lev@gmx.com>
2021-02-07 12:16:44 +0800

    fix pcap deleting function

--- source/csv_generator.py
+++ source/csv_generator.py
@@ -98,7 +98,7 @@ for f in files :
     logs.write("tshark cmd: "+ csv_command+"\n")
     logs.flush()
 
-    remove_file = "sudo rm -rf "+ directory_prefix + file_name
+    remove_file = "rm -rf "+ file_name
     try:
       os.system(csv_command)
       print(str(count) + " of " + str(total) + " completed!")
